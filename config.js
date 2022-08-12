91a4d49ceff80f5aae2da533b9f16cfa3a420c03
GopalAsu <91899516+VarrelKun@users.noreply.github.com>
2022-08-12 22:48:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -41,7 +41,7 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = 'FUZEBOT'
+  var sticker_name = 'PIANBOT'
   var sticker_author = '_'
 } else {
   var sticker_name = stickerpack.spackname
@@ -59,9 +59,9 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = sticker_name
 global.author = sticker_author
-global.wm = 'FUZEBOT'
+global.wm = 'PIANBOT'
 
-global.multiplier = 1000 // The higher, The harder levelup
+global.multiplier = 100 // The higher, The harder levelup
 
 global.rpg = {
   emoticon(string) {
