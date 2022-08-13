ff79fa82735406fcfeda575cf829e0299df0018f
GopalAsu <91899516+VarrelKun@users.noreply.github.com>
2022-08-13 18:14:02 +0700

    Update _role.js

--- plugins/_role.js
+++ plugins/_role.js
@@ -1,5 +1,4 @@
-/*let handler = m => m
-
+let handler = async (m, { command, usedPrefix, args }) => {
 handler.before = function (m) {
   let user = global.db.data.users[m.sender]
         let role = (user.level <= 20) ? 'Beginner'
@@ -13,36 +12,7 @@ handler.before = function (m) {
           : 'Legends'
   user.role = role
   return true
+    }
 }
 
-module.exports = handler*/
-
-const roles = {
-  /*
-  'Role Name': <Minimal Level To Obtain this Role>
-  */
-  'Beginner': 0, 
-  'Early Soldier': 5,
-  'Commander': 10,
-  'Initial Commander' 15,
-  'Commander Elite': 20,
-  'Initial Commander Elit': 25,
-  'Commander Elite Hero': 30,
-  'The Commander Elite Super Strong Hero': 40,
-  'The Commander Elite Super Strong Shadow Hero': 50,
-  'The Commander Legends Shadow Hero': 60,
-  'The Commander Super Legends Shadow Hero': 70,
-  'The Commander Super Legends Shadow Hero': 80,
-  'The Strongest Legend': 90,
-  'The Hero Of The Legend\'s Strongest': 100
-}
-
-export default handler = {
-  before (m) {
-    let user = global.db.data.users[m.sender]
-    let level = user.level
-    let role = (Object.entries(roles).sort((a, b) => b[1] - a[1]).find(([,minLevel]) => level >= minLevel) || Object.entries(roles)[0])[0]
-    user.role = role
-    return true
-  }
-}
+export default handler
